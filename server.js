676ca4a04f422b70be299906d484be3b6ed1445f
awalone <migoyeng@gmail.com>
2020-03-13 11:11:19 +0800

    add REST API exercises and user

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ connection.once('open', () => {
 const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
 
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
